a7771a2fd68c021858652cd03366cd3f4b1727ce
Davi Ximenes <davixmnsl@gmail.com>
2022-11-23 15:33:15 -0300

    aula 3

--- src/aula_2/Main.java
+++ src/aula_2/Main.java
@@ -1,3 +1,5 @@
+package aula_2;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ src/aula_3/Main.java
@@ -0,0 +1,22 @@
+package aula_3;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean pcd = false;
+        char primeiraLetra = 'P';
+        String nome = "Paulo";
+        int idade = 23;
+        double altura = 1.67;
+        String endereco = "rua jose antonio de jesus 122 fortaleza CE";
+        double peso = 59.65;
+
+        System.out.println("Dados do cliente:");
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(peso);
+        System.out.println(endereco);
+        System.out.println(primeiraLetra);
+        System.out.println(pcd);
+    }
+}
