30893848e1073d0f76e71da431da7edf932b0abf
Davi Ximenes <davixmnsl@gmail.com>
2022-11-25 18:59:56 -0300

    aula 6

---
+++ src/aula_6/Main.java
@@ -0,0 +1,13 @@
+package aula_6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String texto = scanner.nextLine();
+
+        System.out.println(texto);
+    }
+}
