016db004ff56538b074e9f49ddd362c7b34d719e
Davi Ximenes <davixmnsl@gmail.com>
2022-12-01 17:29:52 -0300

    aula 12

---
+++ src/aula_12/Exemplo2.java
@@ -0,0 +1,10 @@
+package aula_12;
+
+public class Exemplo2 {
+    public static void main(String[] args) {
+        int[] numeros = {3, 6 , 1, 9};
+
+        int indiceDoUltimo = numeros.length - 1;
+        System.out.println(numeros[indiceDoUltimo]);
+    }
+}
