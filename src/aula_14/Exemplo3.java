354ee45d2d8d29cc306b04a8662f5cc9366e36ce
Davi Ximenes <davixmnsl@gmail.com>
2022-12-14 20:29:20 -0300

    aula 14

---
+++ src/aula_14/Exemplo3.java
@@ -0,0 +1,11 @@
+package aula_14;
+
+public class Exemplo3 {
+    public static void main(String[] args) {
+        int[] numeros = {2, 1, 3, 4, 7, 12, 34, 7, 23, 5};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+    }
+}
---
+++ src/aula_14/Exemplo4.java
@@ -0,0 +1,11 @@
+package aula_14;
+
+public class Exemplo4 {
+    public static void main(String[] args) {
+        int[] numeros = {2, 1, 3, 4, 7, 12, 34, 7, 23, 5};
+
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+        }
+    }
+}
