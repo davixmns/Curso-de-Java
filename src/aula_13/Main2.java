72638fd5ada273f8b982b27d8b158ced0cfb5ae0
Davi Ximenes <davixmnsl@gmail.com>
2022-12-14 17:48:39 -0300

    aula 13

---
+++ src/aula_13/Main2.java
@@ -0,0 +1,15 @@
+package aula_13;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int[] numeros = {2, 1, 3, 4, 7, 12, 34, 7, 23, 5};
+
+        int i = 0;
+        int comprimento = numeros.length;
+
+        while (i < comprimento){
+            System.out.print(numeros[i] + " ");
+            i++;
+        }
+    }
+}
